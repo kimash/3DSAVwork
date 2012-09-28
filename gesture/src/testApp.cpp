45a50161c0f61351608644d5a2b3f1d97b5fb7df
Kim Ash <ash.kimberly.b@gmail.com>
2012-09-27 22:05:33 -0400

    added gesture

---
+++ gesture/src/testApp.cpp
@@ -0,0 +1,28 @@
+#include "testApp.h"
+
+void testApp::setup() {
+	ofSetVerticalSync(true);
+	openni.setup();
+	image.setup(&openni);
+	depth.setup(&openni);
+	user.setup(&openni);
+	openni.registerViewport();
+	openni.setMirror(true);
+    applause.loadSound("applause-9s.mp3");
+}
+
+void testApp::update(){
+	openni.update();
+	image.update();
+	depth.update();
+	user.update();
+}
+
+void testApp::draw(){
+	ofBackground(0);
+	ofSetColor(255);
+	ofScale(.75, .75, .75);
+	depth.draw(0, 0, 640, 480);
+	image.draw(640, 0, 640, 480);
+	user.draw();
+}
---
+++ gesture/src/testApp.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenNI.h"
+
+class testApp : public ofBaseApp{
+	
+public:
+	void setup();
+	void update();
+	void draw();
+	
+	ofxOpenNIContext openni;
+	ofxDepthGenerator depth;
+	ofxImageGenerator image;
+	ofxUserGenerator user;
+    ofSoundPlayer applause;
+};
